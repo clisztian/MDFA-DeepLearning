b3664e6082d1cdd5fc67c584df501e2896bca907
lisztian <clisztian@gmail.com>
2018-04-03 12:06:55 +0200

    first commit

---
+++ src/main/java/ch/imetrica/mdlfa/labeling/Labelizer.java
@@ -0,0 +1,62 @@
+package ch.imetrica.mdlfa.labeling;
+import java.io.Serializable;
+
+import ch.imetrica.mdfa.series.TargetSeries;
+import ch.imetrica.mdfa.series.TimeSeries;
+
+/**
+ * 
+ * Interface to create a labeled series for machine learning 
+ * applications. 
+ * 
+ * There are three categories of labeling processes that this interface
+ * will offer:
+ * 
+ * 1) Observational labeling: every time series observation is labeled
+ * 2) Fixed Period labeling: every period (day, week, etc) is labeled
+ * 3) Regime labeling: every regime change is labeled
+ * 
+ * Additionally, there are two types of labels that can be c
+ * 
+ * 
+ * 
+ * @author Christian D. Blakely (clisztian@gmail.com)
+ *
+ */
+public interface Labelizer extends Serializable {
+
+	public enum LabelType {
+		
+		OBSERVATIONAL,  /* Every time series observation is labeled */
+		FIXED_PERIOD,   /* Certain fixed-period length labeled */
+		REGIME;		    /* Undetermined length of time labeled */
+	}
+	
+	/**
+	 * 
+	 * With a target series provided, the routine takes either the 
+	 * transformed price data (fractionally differenced data or the raw
+	 * price data from the target series and constructs a labeled timeseries
+	 * for each observation in the series. 
+	 * 
+	 * If the time series observation has no label, the value will be null
+	 * at that timestamp observation 
+	 * 
+	 * 
+	 * @param series A TargetSeries with original raw price data and 
+	 * transformed data 
+	 * @return
+	 *   A TimeSeries with labels in the form of one-hot vectors for 
+	 *   classifying observations
+	 */
+	public TimeSeries<double[]> labelTimeSeries(TargetSeries series);
+	
+    /**
+     * Returns the LabelType of the given labelizer. 
+     * 
+     * @return
+     *   A LabelType value of the given labeling rule
+     */
+	public LabelType getLabelType();
+	
+}
---
+++ src/main/java/ch/imetrica/mdlfa/labeling/SymmetricLabelizer.java
@@ -0,0 +1,103 @@
+package ch.imetrica.mdlfa.labeling;
+
+import ch.imetrica.mdfa.series.TargetSeries;
+import ch.imetrica.mdfa.series.TimeSeries;
+import ch.imetrica.mdfa.series.TimeSeriesEntry;
+
+/**
+ * 
+ * This time series labeler constructs a labeled 
+ * time series for deep learning applications based
+ * on the help of a defined target symmetric filter.
+ * For a given frequency <code> omega_0 \in (0, pi)</code>,
+ * this labeler outputs at each time-series 
+ * observation a one-hot vector of length two where the value of (1, 0)  
+ * signals an uptrend in the underlying (price) series and (0, 1) in the 
+ * case of a downtrend.
+ *  
+ * The constructor initializes a labelizer with 
+ * a given target frequency and a symmetric filter length.
+ * All observations in a given TargetSeries will then be labeled except for the
+ * first and last L observations, where L is the filter length. These 
+ * time series observations will be filled with null values 
+ * 
+ * 
+ * @author lisztian
+ *
+ */
+public class SymmetricLabelizer implements Labelizer {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private LabelType labelType = LabelType.OBSERVATIONAL;
+	private int L; 
+	private double omega0;
+	private double[] symmetricaCoeffs;
+	
+	public SymmetricLabelizer(double omega0, int L) {
+		
+		this.omega0 = omega0;
+		this.L = L; 	
+	}
+	
+	
+	public SymmetricLabelizer computeSymmetricFilter() {
+					
+		double sum = 0; 
+		symmetricaCoeffs = new double[L+1];
+		symmetricaCoeffs[0] = omega0/Math.PI; 
+		sum = symmetricaCoeffs[0];
+		
+		for(int i=1;i<=L;i++) {
+			symmetricaCoeffs[i] = (1.0/Math.PI)*Math.sin(omega0*i)/(double)i; 
+			sum = sum + symmetricaCoeffs[i];
+		} 
+		
+		sum = sum+(sum-symmetricaCoeffs[0]);
+		for(int i=0;i<=L;i++) {
+			symmetricaCoeffs[i] = symmetricaCoeffs[i]/sum;
+		}		
+		
+		return this;
+	}
+	
+
+	@Override
+	public TimeSeries<double[]> labelTimeSeries(TargetSeries series) {
+		
+		int N = series.size();
+		double sum = 0;
+		
+		TimeSeries<double[]> label = new TimeSeries<double[]>();
+
+		for(int i = 0; i < L; i++) {
+			label.add(new TimeSeriesEntry<double[]>(series.getTargetDate(i), null));
+		}
+		
+		for(int i = L-1; i < N - L; i++) {
+			
+			sum = 0.0;
+			for(int l=0;l < L; l++) {
+		    	sum = sum + symmetricaCoeffs[l]*series.getTargetValue(i+l);
+		    } 
+			
+		    for(int l=1; l < L; l++) {
+		    	sum = sum + symmetricaCoeffs[l]*series.getTargetValue(i-l);
+		    }
+		    
+		    double[] output = {0.0, 0.0};
+		    output[sum > 0 ? 0 : 1] = 1.0;
+		    
+		    label.add(new TimeSeriesEntry<double[]>(series.getTargetDate(i), output));
+		}
+		
+		for(int i = N - L; i < N; i++) {
+			label.add(new TimeSeriesEntry<double[]>(series.getTargetDate(i), null));
+		}
+
+		return label;
+	}
+	
+}
